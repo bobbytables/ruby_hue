f4019d2f07b0a025e9989631dc22a2447fdcc18e
Robert Ross <robert@creativequeries.com>
2013-03-26 00:55:55 -0700

    Add initial light class to represent different bulbs.

--- lib/ruby_hue.rb
+++ lib/ruby_hue.rb
@@ -1,6 +1,7 @@
 require "ruby_hue/version"
 require "ruby_hue/client"
 require "ruby_hue/configuration"
+require "ruby_hue/light"
 
 module RubyHue
   def self.configuration
---
+++ lib/ruby_hue/light.rb
@@ -0,0 +1,5 @@
+module RubyHue
+  class Light
+
+  end
+end
