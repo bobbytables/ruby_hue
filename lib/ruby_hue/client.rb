d2b5e9a4b4a737409ee31c27e581414dbadafa00
Robert Ross <robert@creativequeries.com>
2013-03-28 00:48:20 -0700

    Add get_and_parse method to retrieve and parse json body.

--- lib/ruby_hue/client.rb
+++ lib/ruby_hue/client.rb
@@ -9,5 +9,10 @@ module RubyHue
     def initialize(username)
       @username = username
     end
+
+    def self.get_and_parse(*args)
+      response = get(*args)
+      MultiJson.load(response.body)
+    end
   end
 end
