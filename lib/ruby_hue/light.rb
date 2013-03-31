271bc5ed5ec7464cdc8353eae6b725057733d9b0
Robert Ross <robert@creativequeries.com>
2013-03-30 21:19:43 -0700

    Make lights be able to get their state on initialize.

--- lib/ruby_hue/light.rb
+++ lib/ruby_hue/light.rb
@@ -1,5 +1,22 @@
 module RubyHue
   class Light
+    attr_reader :id, :bridge
 
+    def initialize(id, bridge)
+      @id = id
+      @bridge = bridge
+
+      refresh_state!
+    end
+
+    def state
+      @state
+    end
+
+    def refresh_state!
+      url = bridge.resource_url_for("lights/#{id}")
+      light_attributes = Client.get_and_parse(url)
+      @state = light_attributes["state"]
+    end
   end
 end
--- spec/ruby_hue/light_spec.rb
+++ spec/ruby_hue/light_spec.rb
@@ -1,5 +1,35 @@
 require "spec_helper"
 
 describe RubyHue::Light do
-  describe ""
+  let(:id) { "1" }
+  let(:bridge) { RubyHue::Bridge.new("192.168.100.1", "awesomeusername") }
+  subject(:light) { described_class.new(id, bridge) }
+
+  before do
+    stub_request(:get, bridge.resource_url_for("lights/#{id}"))
+      .to_return(body: fixture("light_1.json"))
+  end
+
+  describe "#initialize" do
+    it "sets the ID" do
+      instance = described_class.new(id, bridge)
+      expect(instance.id).to eq(id)
+    end
+
+    it "sets the bridge" do
+      instance = described_class.new(id, bridge)
+      expect(instance.bridge).to be bridge
+    end
+
+    it "finds the state for the light" do
+      instance = described_class.new(id, bridge)
+      expect(instance.state).to be_kind_of Hash
+    end
+  end
+
+  describe "#refresh_state!" do
+    it "sets the state hash" do
+      expect { light.refresh_state! }.to change { light.state.object_id }
+    end
+  end
 end
