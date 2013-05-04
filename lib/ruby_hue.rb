b60d62a86d7f9a7b6dd61541b441b3d539fa7894
Robert Ross <robert@creativequeries.com>
2013-05-04 13:54:35 -0700

    Move everything into autoloads that can be.

--- lib/ruby_hue.rb
+++ lib/ruby_hue.rb
@@ -1,10 +1,12 @@
 require "ruby_hue/version"
-require "ruby_hue/client"
-require "ruby_hue/model"
-require "ruby_hue/light"
-require "ruby_hue/light/basic_state"
-require "ruby_hue/light/state"
-require "ruby_hue/bridge"
 
 module RubyHue
+  autoload :Client, "ruby_hue/client"
+  autoload :Bridge, "ruby_hue/bridge"
+  autoload :Model, "ruby_hue/model"
+  autoload :Light, "ruby_hue/light"
+  autoload :LightsCollection, "ruby_hue/lights_collection"
 end
+
+require "ruby_hue/light/basic_state"
+require "ruby_hue/light/state"
